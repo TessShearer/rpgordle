15b84458ceb6d28ed8a5a6c8f7cb98e6ab9500fd
TessShearer <shearer.tess@gmail.com>
2026-06-20 22:00:38 -0600

    add some more game info

--- src/views/GameInfoView.vue
+++ src/views/GameInfoView.vue
@@ -2,6 +2,13 @@
   <main class="container py-4">
 
     <section class="info-section mb-5">
+      <h2 class="info-heading mb-3">Game</h2>
+      <div class="">
+        Currently the game consists of 7 rounds. Rounds 1-4 are normal, round 5 adds a random enemy, and round 7 is a boss (long word). I added a testing features square at the bottom that reveals the answer word to you and lets you add an enemy early or switch the enemy out. Here are all current characters/enemies:
+      </div>
+    </section>
+
+    <section class="info-section mb-5">
       <h2 class="info-heading mb-3">Characters</h2>
       <div class="info-grid">
         <div v-for="cls in CLASSES" :key="cls.id" class="info-card">
